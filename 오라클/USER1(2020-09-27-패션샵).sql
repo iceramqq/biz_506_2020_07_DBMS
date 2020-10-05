f0016b745f8366d777683f554efee0d9d346e4e5
iceramqq <iceram22@naver.com>
2020-10-05 16:45:04 +0900

    dbms

--- 오라클/USER1(2020-09-27-패션샵).sql
+++ 오라클/USER1(2020-09-27-패션샵).sql
@@ -3,14 +3,13 @@ DROP TABLE tbl_iolist;
 
 CREATE TABLE tbl_iolist (
     seq	NUMBER		PRIMARY KEY,
-    io_date	VARCHAR2(10)	NOT NULL	,
-    io_time	VARCHAR2(10)	NOT NULL	,
-    io_pname	nVARCHAR2(30)	NOT NULL,	
+    io_date	VARCHAR2(10)	,
+    io_time	VARCHAR2(10)	,
+    io_pname	nVARCHAR2(30)	,	
     io_input	CHAR(1)		,
     io_price	NUMBER		,
     io_quan	NUMBER		,
-    io_total	NUMBER,
-    io_delete	CHAR(1)	DEFAULT '1' 
+    io_total	NUMBER 
 );
 
 CREATE SEQUENCE seq_iolist
