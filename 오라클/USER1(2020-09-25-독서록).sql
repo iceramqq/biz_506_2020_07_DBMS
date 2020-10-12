d79b26604d5fba8b4b0185212e4bdb469db1d842
iceramqq <iceram22@naver.com>
2020-10-12 16:27:51 +0900

    dbms

--- 오라클/USER1(2020-09-25-독서록).sql
+++ 오라클/USER1(2020-09-25-독서록).sql
@@ -1,4 +1,5 @@
 -- USER 독서록 프로젝트
+DROP TABLE tbl_authority;
 DROP TABLE tbl_member;
 
 CREATE TABLE tbl_member (
@@ -7,8 +8,9 @@ CREATE TABLE tbl_member (
     M_NAME	nVARCHAR2(30)		,
     M_TEL	VARCHAR2(30)		,
     M_ADDRESS	nVARCHAR2(125)	,	
-    M_ROLL	VARCHAR2(20)		,
-    Enabled	CHAR(1)	DEFAULT '0',
+    M_ROLE	VARCHAR2(20)		,
+    Enabled	CHAR(1)	DEFAULT '0'
+                CONSTRAINT enable_veri CHECK(enabled = '0' OR enabled = '1'),
     AccountNonExpired	CHAR(1)		,
     AccountNonLocked	CHAR(1)		,
     CredentialsNonExpired	CHAR(1)	,	
@@ -19,7 +21,73 @@ CREATE TABLE tbl_member (
 CREATE TABLE tbl_authority (
     seq	NUMBER		PRIMARY KEY,
     M_USERID	VARCHAR2(30)	NOT NULL	,
-    M_ROLL	VARCHAR2(31)	NOT NULL	
+    M_ROLE	VARCHAR2(31)	NOT NULL	
 );
 CREATE SEQUENCE seq_authority
 START WITH 1 INCREMENT BY 1;
+
+DELETE FROM tbl_member;
+DELETE FROM tbl_authority;
+COMMIT;
+
+SELECT * FROM tbl_member;
+SELECT * FROM tbl_authority;
+
+-- 한개의 
+-- seq 값으로 pk설정을 해두면 sql 작동을 하지 않는다
+insert ALL 
+    INTO tbl_member(m_userid, m_password) VALUES ('user1','1')
+    INTO tbl_member(m_userid, m_password) VALUES ('user2','1')
+    INTO tbl_member(m_userid, m_password) VALUES ('user3','1')
+    INTO tbl_member(m_userid, m_password) VALUES ('user4','1')
+    INTO tbl_member(m_userid, m_password) VALUES ('user5','1')
+SELECT * FROM dual;
+
+insert ALL 
+    INTO tbl_authority(m_userid, m_roll) VALUES ('admin','ADMIN')
+    INTO tbl_authority(m_userid, m_roll) VALUES ('admin1','ADMIN')
+    INTO tbl_authority(m_userid, m_roll) VALUES ('admin2','ADMIN')
+    INTO tbl_authority(m_userid, m_roll) VALUES ('admin3','ADMIN')
+SELECT * FROM dual;
+
+DELETE FROM tbl_authority;
+
+-- 오라클에서 seq pk  칼럼을 가진 table db 다중 insert 문을 수행하기 위해서
+-- 1.
+-- 2.
+-- 3. 서브쿼리 부모 sql에서 seq.next
+-- 47.
+
+INSERT INTO tbl_authority (seq,m_userid,m_role)
+SELECT SEQ_AUTHORITY.NEXTVAL, sub.*FROM
+(
+-- 가상테이블
+SELECT 'user11' as username,'POLE_ADMIN' as authority FROM dual
+UNION ALL
+SELECT 'user11' as username,'POLE_USER' as authority FROM dual
+UNION ALL
+SELECT 'user12' as username,'POLE_ADMIN' as authority FROM dual
+UNION ALL
+SELECT 'user12' as username,'POLE_USER' as authority FROM dual
+) sub;
+
+
+DELETE FROM tbl_member;
+DELETE FROM tbl_authority;
+COMMIT;
+
+SELECT * FROM tbl_member;
+SELECT * FROM tbl_authority;
+
+SELECT * FROM tbl_member M
+    LEFT JOIN tbl_authority A
+        ON m.m_userid = a.m_userid;
+
+UPDATE tbl_member SET enabled = 1 WHERE m_userid = 'user';
+
+
+
+
+
+
+
---
+++ 오라클/USER1(2020-10-12-독서록).sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE tbl_readbook (
+r_seq	NUMBER		PRIMARY KEY,
+r_book_seq	NUMBER	NOT NULL	,
+r_date	VARCHAR2(10)		,
+r_stime	VARCHAR2(10)		,
+r_etime	VARCHAR2(10)		,
+r_comment	nVARCHAR2(2000)		
+
+);
+CREATE SEQUENCE SEQ_READBOOK
+START WITH 1 INCREMENT BY 1;
+
+SELECT * FROM tbl_readbook;
